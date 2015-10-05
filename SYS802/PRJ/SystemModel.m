400694be15e719d33afc393c91e72b09fd3f2dc7
AdrienVassal <vassal.adrien.38@gmail.com>
2015-10-05 08:42:37 -0400

    [SYS802][GPA430]
    [SYS802] : Création du modèle + contrôleur sur matlab
    Ajout de l'article du professeur sur la méthode B
    [GPA430] : Modification des noms sur Lingo

--- SYS802/PRJ/SystemModel.m
+++ SYS802/PRJ/SystemModel.m
@@ -1,3 +1,47 @@
-clear all; close all; clc
+clear; close all; clc
 %%
-% Description du modele utilisé
+% Point de fonctionnement 1
+Ka = -7817.5;
+s1 = -30.5122;
+s2 = 31.3407;
+s3 = 184.378;
+% Point de fonctionnement 2
+% Ka = -8515.2;
+% s1 = -30.5122;
+% s2 = 31.3407;
+% s3 = 184.378;
+% Définition du système
+P1 = series(Ka*tf([0 1],[1 s2]),tf([0 1],[1 s3]));
+P2 = tf([0 1],[1 s1]);
+P  = series(P1,P2);
+% Définition du gain k1
+k1 = 2;
+% Définition du correcteur H
+c  = 1;
+s0 = 1;
+q  = 1;
+H  = c*P1;
+for i = 1 : q
+    H = series(H,tf([0 1],[1 s0]));
+end
+% Définition du correcteur J1
+w1 = 1;
+J1 = tf([0 w1],[1 w1]);
+% Définition du correcteur J2
+z  = [1 1];
+p  = [1 1];
+J2 = 1;
+for i = 1 : 2
+    J2 = series(J2,tf([1 z(i)],[1 p(i)]));
+end
+% Définition du correcteur J3
+k  = 1;
+w2 = 1;
+J3 = 1;
+for i = 1:k
+    J3 = series(J3,tf([0 w2],[1 w2]));
+end
+% Définition du correcteur total
+C = series(series(series(k1*H, J1),J2),J3);
+% Définition de la boucle ouverte
+J = series(C,P);
